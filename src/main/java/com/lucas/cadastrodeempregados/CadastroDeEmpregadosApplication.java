960b07b630f9cd279b31f5beae792495ef789a22
Lucas Cadaval <cadavallfc@gmail.com>
2025-04-17 22:09:51 -0300

    feat: Primeiro commit

---
+++ src/main/java/com/lucas/cadastrodeempregados/CadastroDeEmpregadosApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.cadastrodeempregados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeEmpregadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeEmpregadosApplication.class, args);
+	}
+
+}
